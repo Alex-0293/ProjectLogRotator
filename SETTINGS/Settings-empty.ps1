478087c62f3c63dc876b6752491a1c3297ca4353
Alex-0293 <1928311@tuta.io>
2020-05-04 19:40:16 +0300

    Add script Init and Finish.

--- SETTINGS/Settings-empty.ps1
+++ SETTINGS/Settings-empty.ps1
@@ -1,3 +1,4 @@
+# Rename this file to Settings.ps1
 #### Script params    
     [array] $global:FoldersToApplyPath  = @()         # Folders where searching
   
@@ -7,3 +8,11 @@
     [Int16] $Global:DaysToRotateLog  = 30                                           # Days count to save logs.
     [string]$Global:LogFilePath      = "$ProjectPath\LOGS\$ScriptName.log"          # Path to this script log file.
     [array] $Global:ExcludeFiles     = @("Transcript.log")
+
+    [bool] $Global:LocalSettingsSuccessfullyLoaded = $true
+
+# Error trap
+trap {
+    $Global:LocalSettingsSuccessfullyLoaded = $False
+    exit 1
+}
